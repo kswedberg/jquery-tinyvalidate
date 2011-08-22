30f6c2d449c5f4abdba8bc3d49a560c0964394a2
Karl Swedberg <kswedberg@gmail.com>
2011-08-22 11:08:20 -0400

    added tests for validate on blur

--- test/tests.js
+++ test/tests.js
@@ -1,7 +1,7 @@
 
 module('defaults', {
   setup: function() {
-    this.frm = $('form.full');
+    this.frm = $('form').eq(0);
     this.frm.tinyvalidate();
   }
 });
@@ -12,7 +12,13 @@ test('submit', function() {
   equal(this.frm.find('.error').length, 3, '3 errors after submit');
 });
 
+test('blur', function() {
+  this.frm.find(':input').trigger('blur');
+  equal(this.frm.find('.error').length, 3, '3 errors after blurring all');
+});
+
 test('removeErrors', function() {
+  this.frm.triggerHandler('submit');
   equal(this.frm.find('.error').length, 3, '3 errors after submit');
   this.frm.tinyvalidate('removeErrors');
   equal(this.frm.find('.error').length, 0, '0 errors after tinyvalidate("removeErrors")');
